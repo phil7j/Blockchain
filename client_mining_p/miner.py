cc4297b810673f732abceec7e703c9cb8b97caad
Philip Johnson <phil7j@gmail.com>
2019-12-04 16:55:58 -0800

    setup flask server, mining function wip

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,7 +13,11 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +31,9 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == "0" * 6
 
 
 if __name__ == '__main__':
@@ -48,7 +54,7 @@ if __name__ == '__main__':
         r = requests.get(url=node + "/last_block")
         # Handle non-json response
         try:
-            data = r.json()
+            last_block = r.json()
         except ValueError:
             print("Error:  Non-json response")
             print("Response returned:")
@@ -56,7 +62,7 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        new_proof = proof_of_work(last_block)
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
